c9f70fcfbd0afc6164f1e065e7c1bc2000caf0fc
dapait03 <dapait03@hs-esslingen.de>
2023-04-21 12:03:32 +0200

    simplified code

--- mobile-ux-chat/src/App.js
+++ mobile-ux-chat/src/App.js
@@ -1,17 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import TopAppBar from "./components/TopAppBar";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import GroupChat from "./components/GroupChat";
 
 export default function App() {
   const [changedPath, setPath] = useState("");
-
-  const pull_path = (path) => {
-    setPath(path);
-  };
 
   return (
     <div className="App">
@@ -19,7 +15,7 @@ export default function App() {
     <TopAppBar path={window.location.pathname}/>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage path={pull_path}/>} />
+        <Route path="/register" element={<RegisterPage setPath={setPath}/>} />
         <Route path="/chat" element={<GroupChat />} />
       </Routes>
     </BrowserRouter>
