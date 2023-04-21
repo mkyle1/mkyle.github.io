e3ace8e82ecc7a84da62d08a05ee9400678e5a0c
dapait03 <dapait03@hs-esslingen.de>
2023-04-21 12:11:44 +0200

    Cleaning code

--- mobile-ux-chat/src/components/TopAppBar.js
+++ mobile-ux-chat/src/components/TopAppBar.js
@@ -7,53 +7,49 @@ import AppBar from "@mui/material/AppBar";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import SettingsIcon from '@material-ui/icons/Settings';
 import Settings from "./Settings";
-import { Link } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
 
 const TopAppBar = (props) => {
   const [backButton, setBackButton] = useState(false);
   const [settingsButton, setSettingsButton] = useState(false);
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
-    const currentPath = props.path;
-    setBackButton(currentPath === "/register");
-    setSettingsButton(currentPath === "/chat");
-}, []);
+    setBackButton(location.pathname === "/register");
+    setSettingsButton(location.pathname === "/chat");
+}, [location.pathname]);
 
-  const handleClickOpen = () => {
+  const handleBackClick = () => {
+    props.setPath("/");
+  };
+
+  const handleSettingsOpen = () => {
     setSettingsOpen(true);
   };
-  const settingsClose = () => {
+
+  const handleSettingsClose = () => {
     setSettingsOpen(false);
   };
 
   return (
-    <Paper
-      sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: "20" }}
-      elevation={1}
-    >
+    <Paper sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: "20" }} elevation={1}>
       <AppBar position="static" sx={{ fontSize: 72, backgroundColor: "#007ACC" }}>
         <Toolbar>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1}}>
             Chat App
           </Typography>
           {backButton && (
-          <IconButton component={Link} to="/"
-            color="inherit"
-            aria-label="back"
-          >
-            <ArrowBackIcon fontSize="large"/>
-          </IconButton>)}
+            <IconButton component={Link} to="/" color="inherit" aria-label="back" onClick={handleBackClick}>
+              <ArrowBackIcon fontSize="large"/>
+            </IconButton>
+          )}
           {settingsButton && (
-          <IconButton
-            color="inherit"
-            aria-label="menu"
-            onClick={handleClickOpen}
-          >
-            <SettingsIcon fontSize="large"/>
-          </IconButton>)}
-          <Settings open={settingsOpen} onClose={settingsClose}/>
+            <IconButton color="inherit" aria-label="menu" onClick={handleSettingsOpen}>
+              <SettingsIcon fontSize="large"/>
+            </IconButton>
+          )}
+          <Settings open={settingsOpen} onClose={handleSettingsClose}/>
         </Toolbar>
       </AppBar>
     </Paper>
