a92db57a2a00ee56243f2164e0ce5fc415e06a64
Kyle Mezger <kylemezger@aol.de>
2023-06-08 02:08:00 +0200

    Update GroupChat Page style, fix centering etc

--- mobile-ux-chat/src/components/GroupChat.js
+++ mobile-ux-chat/src/components/GroupChat.js
@@ -36,41 +36,61 @@ function GroupChat() {
   }
 
   return (
-    <div style={{height: '90vh', paddingBottom: '100px'}}>
-      {messages.map((message, index) =>
-        <div key={index} sx={{ marginBottom: '30px'}}>
-        <MessageCard
-          usernickname={message.usernickname}
-          time={message.time ? timeSort(message.time) : ""}
-          text={message.text}
-          isOwnMessage={message.usernickname === currentUser}
-        >
-        </MessageCard>
-        </div>
-      )}
-      <div className="input-and-send" style={{display: 'flex', position: 'fixed', bottom: '0', alignItems: 'center', marginBottom: '30px'}}>
-        <IconButton color="primary" aria-label="upload picture" component="label">
-          <input hidden accept="image/*" type="file" />
-          <PhotoCamera />
-        </IconButton>
-        <TextField
-          className="message-input"
-          type="text"
-          value={message}
-          onChange={handleMessageChange}
-          placeholder="Type your message here..."
-          sx={{width: 'auto'}}
-        />
-        <Button 
-          variant="contained"
-          onClick={handleSendMessage}
-          color="primary"
-          className="button"
-          sx={{marginLeft: '10px'}}
-        >
-            Send
-        </Button>
+    <div style={{height: '110vh',
+                 marginTop: '4vh',
+                 paddingTop: '1vh',
+                 paddingBottom: '100px',
+                 justifyContent: 'center',
+                 overflow: 'hidden',
+                 display: "flex",
+                 flexDirection: 'column'}}>
+      <div className="messages" style={{overflowY: 'scroll', paddingBottom: '27vh'}}>
+        {messages.map((message, index) =>
+          <div key={index} sx={{ marginBottom: '30px'}}>
+            <MessageCard
+              usernickname={message.usernickname}
+              time={message.time ? timeSort(message.time) : ""}
+              text={message.text}
+              isOwnMessage={message.usernickname === currentUser}
+            >
+            </MessageCard>
+          </div>
+        )}
       </div>
+      <div className="inputs" style={{width: '100%',
+                                      display: 'flex',
+                                      justifyContent: 'center'}}>
+        <div className="frosted-glass-panel" style={{display: 'flex', 
+                                                    position: 'fixed',
+                                                    justifyContent: 'center',
+                                                    bottom: '0',
+                                                    alignItems: 'center',
+                                                    marginBottom: '30px',
+                                                    padding: '5 5 0 0',}}>
+          <IconButton color="primary" aria-label="upload picture" component="label">
+            <input hidden accept="image/*" type="file" />
+            <PhotoCamera />
+          </IconButton>
+            <TextField
+              className="message-input"
+              type="text"
+              value={message}
+              onChange={handleMessageChange}
+              placeholder="Message"
+              sx={{width: 'auto'}}
+            />
+            <Button 
+              variant="contained"
+              onClick={handleSendMessage}
+              color="primary"
+              className="button"
+              sx={{marginLeft: '10px'}}
+            >
+                Send
+            </Button>
+        </div>
+      </div>
+    
     </div>
   );
 }
