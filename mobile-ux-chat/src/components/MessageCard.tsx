e44de6c69a932f08c27dff73ae3208498c9f0848
Kyle Mezger <kylemezger@aol.de>
2023-06-08 18:14:38 +0200

    Update MessageCard to frosted glass look

--- mobile-ux-chat/src/components/MessageCard.tsx
+++ mobile-ux-chat/src/components/MessageCard.tsx
@@ -25,21 +25,67 @@ const MessageBox = styled(Box)({
 
     
 function MessageCard({ usernickname, text, photoid, time, isOwnMessage }: PropsWithChildren<Props>): JSX.Element {
-  const limitedText = text ?? ''; // use the entire text message
+  let limitedText = text ?? ''; // use the entire text message
+  if (limitedText.length === 0) {
+    //return <></>; // don't render anything if there's no text
+  }
   const rightLeft = isOwnMessage ? 'right' : 'left';
   const boxMargin = isOwnMessage ? '5px 10px 5px 10px': '10px';
   const imageSrc = "https://www2.hs-esslingen.de/~melcher/map/chat/api/?request=getphoto&token=" + localStorage.getItem("loginToken") + "&photoid=" + photoid;
   let image;
   if (photoid) {
-    image = <img src={imageSrc} alt="" style={{maxWidth: '100%', maxHeight: '100%'}}/>;
+    image = <img src={imageSrc} alt="" style={{maxWidth: '100%',
+                                               maxHeight: '100%',
+                                              borderRadius: '8px'}}/>;
   }
 
   return (
-    <Box sx={{ display: 'inline-block', margin: boxMargin, textAlign: 'left', float: rightLeft , maxWidth: '80%'}}>
-      <StyledBox sx={{ textAlign: 'left', padding: '0px 5px 0px 5px', fontSize: '20px', fontWeight: 'bold', borderRadius: '10px 10px 0px 0px' }}>{usernickname}</StyledBox>
-      <MessageBox sx={{ fontSize: '20px', wordBreak: 'break-word' }}>{limitedText}</MessageBox>
-      <MessageBox sx={{ fontSize: '20px', wordBreak: 'break-word' }}>{image}</MessageBox>
-      <StyledBox sx={{ textAlign: 'right', padding: '0px 5px 0px 5px',backgroundColor: "white", borderTop: "1px solid gray", borderRadius: '0px 0px 10px 10px' }}> {time} </StyledBox>
+    <Box className="frosted-glass-panel" sx={{ display: 'inline-block',
+               margin: boxMargin,
+               padding: '0',
+               textAlign: 'left',
+               float: rightLeft ,
+               maxWidth: '80%'}}>
+        <div className='username' style={{fontWeight: 'bold',
+                                          borderBottom: '1px solid grey',
+                                          padding: '0 0.4em 0.1em 0.4em'}}>
+          {usernickname}
+        </div>
+        <div className='message-text' style={{fontWeight: 'normal',
+                                              paddingBottom: '0.2em',
+                                              wordBreak: 'break-word',
+                                              padding: '0 0.4em 0 0.4em'}}>
+          {limitedText}
+        </div>
+        <div className='message-image'>
+          {image}
+        </div>
+        <div className='message-date' style={{fontWeight: 'lighter',
+                                              borderTop: '1px solid grey',
+                                              padding: '0 0.4em 0 0.4em'}}>
+          {time}
+        </div>
+      {/* <StyledBox sx={{ textAlign: 'left',
+                       fontSize: '20px',
+                       fontWeight: 'bold',
+                       borderRadius: '10px 10px 0px 0px' }}>
+        {usernickname}
+      </StyledBox>
+      <MessageBox sx={{ fontSize: '20px',
+                        wordBreak: 'break-word' }}>
+        {limitedText}
+      </MessageBox>
+      <MessageBox sx={{ fontSize: '20px',
+                        wordBreak: 'break-word' }}>
+        {image}
+      </MessageBox>
+      <StyledBox sx={{ textAlign: 'right',
+                       padding: '0px 5px 0px 5px',
+                       backgroundColor: "white",
+                       borderTop: "1px solid gray",
+                       borderRadius: '0px 0px 10px 10px' }}>
+        {time}
+      </StyledBox> */}
     </Box>
   );
 }
